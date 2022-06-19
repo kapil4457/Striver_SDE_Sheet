bc0b2938c2cd341ce7c42a73668fdeb9898b71fc
Kapil Soni <kapilsoni54768161@gmail.com>
2022-06-19 15:36:42 +0530

    Day 2 Completed

---
+++ Day 2/count_inversions.cpp
@@ -0,0 +1,114 @@
+// Coding Ninja : https://www.codingninjas.com/codestudio/problems/count-inversions_615?leftPanelTab=0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long getInversions(long long *arr, int n)
+{
+    // Write your code here.
+
+    //----------Merge Sort-------------
+    long long merge(long long arr[], int left, int mid, int right)
+    {
+        int i = left, j = mid, k = 0;
+        long long invCount = 0;
+        int temp[(right - left + 1)];
+
+        while ((i < mid) && (j <= right))
+        {
+            if (arr[i] <= arr[j])
+            {
+                temp[k] = arr[i];
+                ++k;
+                ++i;
+            }
+            else
+            {
+                temp[k] = arr[j];
+                invCount += (mid - i);
+                ++k;
+                ++j;
+            }
+        }
+
+        while (i < mid)
+        {
+            temp[k] = arr[i];
+            ++k;
+            ++i;
+        }
+
+        while (j <= right)
+        {
+            temp[k] = arr[j];
+            ++k;
+            ++j;
+        }
+
+        for (i = left, k = 0; i <= right; i++, k++)
+        {
+            arr[i] = temp[k];
+        }
+
+        return invCount;
+    }
+
+    // Function to split two subarrays and then merge them and count inversions.
+    long long mergeSort(long long arr[], int left, int right)
+    {
+        long long invCount = 0;
+
+        if (right > left)
+        {
+            int mid = (right + left) / 2;
+
+            /*
+                Divide the array into two parts
+                total inversion count will be the
+                sum of 'INVCOUNT' of left part +
+                'INVCOUNT' of right part + 'INVCOUNT' of
+                their combined part.
+            */
+            invCount = mergeSort(arr, left, mid);
+            invCount += mergeSort(arr, mid + 1, right);
+
+            // Merge both parts and count their combined inversions.
+            invCount += merge(arr, left, mid + 1, right);
+        }
+        return invCount;
+    }
+
+    long long getInversions(long long arr[], int n)
+    {
+        return mergeSort(arr, 0, n - 1);
+
+        //-------- Brute force------------
+        // int nums = 0;
+        //     for(int i = 0 ; i < n ; ++i){
+        //         for(int j =  i+1 ; j <  n;++j){
+        //             if(arr[i] > arr[j]){
+        //                ++nums;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // return nums;
+    }
+
+    int main()
+
+    {
+        long long arr[] = {52244275,
+                           123047899,
+                           493394237,
+                           922363607,
+                           378906890,
+                           188674257,
+                           222477309,
+                           902683641,
+                           860884025,
+                           339100162};
+
+        return 0;
+    }
+}
---
+++ Day 2/rotate_matrix.cpp
@@ -0,0 +1,14 @@
+// Leetcode : https://leetcode.com/problems/rotate-image/
+// Coding Ninja :
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        reverse(matrix.begin(), matrix.end());
+        for (int i = 0; i < matrix.size(); i++)
+            for (int j = 0; j < i; j++)
+                swap(matrix[i][j], matrix[j][i]);
+    }
+};
