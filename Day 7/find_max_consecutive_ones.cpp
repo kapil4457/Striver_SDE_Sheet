b35a7154e7467b055778c2425d453db60a2afe63
Kapil Soni <kapilsoni54768161@gmail.com>
2022-06-27 04:43:06 +0530

    find_max_consecutiive_ones

---
+++ Day 7/find_max_consecutive_ones.cpp
@@ -0,0 +1,21 @@
+// LeetCode : https://leetcode.com/problems/max-consecutive-ones/
+
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int ones = 0, mx = 0;
+        for (auto num : nums)
+        {
+            if (num == 0)
+            {
+                mx = max(mx, ones);
+                ones = 0;
+            }
+            else
+                ones++;
+        }
+        return max(mx, ones);
+    }
+};
